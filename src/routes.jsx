2269aa4156c989971f52207799d91391c094c22b
WParedes2007 <wparedes-2020379@kinal.edu.gt>
2025-05-25 16:58:31 -0600

    Initial

--- src/routes.jsx
+++
@@ -1,8 +0,0 @@
-import { DashboardPage } from "./pages/dashboard/DashBoardPage"
-
-const routes = [
-    //{path: '/aut', element: },
-    {path: '/*', element: <DashboardPage/>}
-]
-
-export default routes;
